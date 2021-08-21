9120d742212544a4ca2cfc99d0d8828041072f29
kohenro19 <kohenro19@gmail.com>
2021-08-21 10:04:07 +0900

    20210821r1

--- search.py
+++ search.py
@@ -1,15 +1,18 @@
+import os
 import pandas as pd
 import eel
+import shutil
 
 ### デスクトップアプリ作成課題
-def kimetsu_search(word):
-    # 検索対象取得
-    df=pd.read_csv(eel.save_csv_js())
-    source=list(df["name"])
+def kimetsu_search(word, csv_path):
+
+     # 検索対象取得
+    df = pd.read_csv("./source.csv")
+    source = list(df["name"])
 	
     # 検索
     if word in source:
-        eel.view_log_js("『{}』はありません".format(word))
+        eel.view_log_js("『{}』はあります".format(word))
 
     else:
         eel.view_log_js("『{}』はありません".format(word))
@@ -23,6 +26,9 @@ def kimetsu_search(word):
 
     # CSV書き込み
     df=pd.DataFrame(source,columns=["name"])
-    df.to_csv("./source.csv",encoding="utf_8-sig")
+    df.to_csv("./source.csv", encoding="utf_8-sig")
     print(source)
 
+    if not os.path.exists(csv_path):
+        os.mkdir(csv_path)
+        shutil.copy2("./source.csv", "./" + csv_path + "/source.csv")
